096fa515d59c5d07322b5366bbd726ae8f5b1981
Igor Korchynskyi <igor.korchynskyi@clarivate.com>
2022-10-04 13:12:45 +0200

    feat!: update rundeck API library to work with the recent rundeck versions

--- src/main/resources/rundeck/ExecuteJob.py
+++ src/main/resources/rundeck/ExecuteJob.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2019 XEBIALABS
+# Copyright 2022 XEBIALABS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -7,78 +7,77 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-from org.rundeck.api import RundeckClient
-from org.rundeck.api import RunJobBuilder
-from org.rundeck.api import OptionsBuilder
-from org.rundeck.api.domain import RundeckExecution
 
+from datetime import timedelta
+from time import sleep
+from org.rundeck.client import RundeckClient     # type: ignore
+from org.rundeck.client.api.model import JobRun  # type: ignore
 
-# Intialize variables and objects
-rdUsername  = rundeckServer['username']
-rdPassword  = rundeckServer['password']
-rdUrl       = rundeckServer['url']
+# Initialize variables and objects
+rdUsername = rundeckServer['username']
+rdPassword = rundeckServer['password']
+rdUrl = rundeckServer['url']
 rdAuthToken = rundeckServer['rundeckAuthToken']
-jobOptions  = OptionsBuilder()
-nodeFilters = OptionsBuilder()
-rundeck     = None
+rundeckApi = None
+job = None
 
-# Build the job options 
-for key,value in rundeckJobOptions.iteritems():
-    jobOptions.addOption(key,value)
-
-# Authenticate either using username/apssword or use auth token
+# Authenticate either using username/password or use auth token
+rundeckClientBuilder = RundeckClient.builder().baseUrl(rdUrl)
 if rdAuthToken:
-     rundeck = RundeckClient.builder().url(rdUrl).token(rdAuthToken).build() 
+    rundeckApi = rundeckClientBuilder.tokenAuth(rdAuthToken).build().getService()
 else:
-     rundeck = RundeckClient.builder().url(rdUrl).login(rdUsername, rdPassword).build()
+    rundeckApi = rundeckClientBuilder.passwordAuth(rdUsername, rdPassword).build().getService()
 
+listJobsResult = rundeckApi.listJobs(rundeckProject, None, None, rundeckJobName, rundeckJobGroup).execute()
+if listJobsResult.successful:
+    listJobs = listJobsResult.body()
+    job = listJobs[0] if len(listJobs) > 0 else None
 
-
-# Validate job exist and get the UUID
-# Not using findjobs as that matches a substring and retutns first match... !
-# Job names are not unique hence iterating through list udner the project and finding the matched one
-
-jobToRun  = None
-jobs      = rundeck.getJobs(rundeckProject);
-
-if rundeckJobGroup:
-   rdjobFullName = "%s%s%s" % (rundeckJobGroup,"/",rundeckJobName)
+if job is None:
+    raise TypeError(
+        'No job has been found with name "%s" in project "%s"' %
+        (rundeckJobName, rundeckProject)
+    )
 else:
-    rdjobFullName = rundeckJobName
+    print(
+        'Job "%s" has been found in project "%s"' %
+        (job.getName(), job.getProject())
+    )
 
-
-for job in jobs:
-    if job.getFullName() == rdjobFullName:
-       jobToRun = job
-       break
-
-if jobToRun is None:
-   raise TypeError("No job found with name:\"%s\" in project: \"%s\" \n" % (rdjobFullName, rundeckProject))
-else:
-     print "Job found: %s in project: %s \n" % (jobToRun.getFullName(),rundeckProject)
-
-# Set the Job Options
-rundeckJobName = jobToRun.getFullName()
-runJob         = RunJobBuilder.builder().setJobId(jobToRun.id).setOptions(jobOptions.toProperties()).build()
-
+jobRun = JobRun()
+jobRun.setOptions(rundeckJobOptions)
+runJobResult = rundeckApi.runJob(job.getId(), jobRun).execute()
+if not runJobResult.successful:
+    raise TypeError(
+        'Job "%s" execution attempt has been failed in project "%s"' %
+        (rundeckJobName, rundeckProject)
+    )
 
 # Either wait for execution or fire and forget
-if rundeckdWaitForJob == True :
-  execution           = rundeck.runJob(runJob)
-  rundeckJobStatus    = execution.status.toString()
-  rundeckJobDuration  = execution.duration
-  rundeckExecutionId  = execution.id
-  print "Execution #%s for job %s succeeded\n" % (rundeckExecutionId, rundeckJobName)
+execution = runJobResult.body()
+rundeckExecutionId = execution.getId()
+rundeckJobStatus = execution.getPermalink()
+rundeckJobDuration = -1
+print(
+    'Execution for job "%s" in project "%s" has been started with id "%s"' %
+    (job.getName(), job.getProject(), rundeckExecutionId)
+)
 
-  # Task should fail if output is not SUCCEEDED
-  if execution.status != RundeckExecution.ExecutionStatus.SUCCEEDED:
-     raise TypeError("Execution #%s for job %s FAILED\n" % (rundeckExecutionId, rundeckJobName))
-
-else:
-   execution = rundeck.triggerJob(runJob)
-   rundeckJobStatus   = "%s\n" % execution.url
-   rundeckJobDuration = -1
-   rundeckExecutionId = execution.id
-   print rundeckJobStatus     
-
-
+if rundeckdWaitForJob:
+    while True:
+        getExecutionStateResult = rundeckApi.getExecutionState(rundeckExecutionId).execute()
+        if not getExecutionStateResult.successful:
+            raise TypeError('Failed to get execution state for execution with id "%s"' % rundeckExecutionId)
+        executionState = getExecutionStateResult.body()
+        if executionState.getCompleted():
+            rundeckJobStatus = executionState.getExecutionState()
+            if rundeckJobStatus == 'SUCCEEDED':
+                jobDuration = (executionState.getEndTime().unixtime - executionState.getStartTime().unixtime) / 1000
+                rundeckJobDuration = str(timedelta(seconds=int(jobDuration + 0.5)))
+            else:
+                raise TypeError(
+                    'Execution with id "%s" for job "%s" in project "%s" has "%s" state' %
+                    (rundeckExecutionId, rundeckJobName, rundeckProject, rundeckJobStatus)
+                )
+            break
+        sleep(5)
